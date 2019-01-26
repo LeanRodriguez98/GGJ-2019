772f084b5da863aa61ea7e0d5d7bf8db30150bb7
Unknown <lean1998@hotmail.com>
2019-01-26 11:05:12 -0300

    PlayerMovement

---
+++ GGJ 2019/Assets/Scrpits/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Vector2 position;
+    public float movementSpeed;
+
+	void Start () {
+        position = transform.position;
+	}
+
+    void Update () {
+        Movement();
+
+    }
+
+    private void Movement()
+    {
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            position.y += movementSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+        }
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            position.x += movementSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+        }
+     
+        if (transform.position.x != position.x || transform.position.y != position.y)
+        {
+            transform.position = position;
+        }
+    }
+}
