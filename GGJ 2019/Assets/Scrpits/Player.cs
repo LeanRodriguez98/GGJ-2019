cd95404620b8abde2a5f63246f0bc288a8d314a8
Unknown <lean1998@hotmail.com>
2019-01-26 14:49:33 -0300

    PlayerFixedAnimations

--- GGJ 2019/Assets/Scrpits/Player.cs
+++ GGJ 2019/Assets/Scrpits/Player.cs
@@ -6,9 +6,10 @@ public class Player : MonoBehaviour {
 
     private Vector2 position;
     public float movementSpeed;
-
+    private Animator animator;
 	void Start () {
         position = transform.position;
+        animator = GetComponent<Animator>();
 	}
 
     void Update () {
@@ -21,10 +22,29 @@ public class Player : MonoBehaviour {
         if (Input.GetAxis("Vertical") != 0)
         {
             position.y += movementSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+            animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+            animator.SetFloat("Horizontal", 0);
+
         }
         if (Input.GetAxis("Horizontal") != 0)
         {
             position.x += movementSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+            animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+            animator.SetFloat("Vertical",0);
+
+        }
+
+        if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+        {
+            animator.SetBool("NoMovement", true);
+            animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+            animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+
+        }
+        else
+        {
+            animator.SetBool("NoMovement", false);
+
         }
 
         if (transform.position.x != position.x || transform.position.y != position.y)
@@ -32,24 +52,34 @@ public class Player : MonoBehaviour {
             transform.position = position;
         }
 
-        if (Input.GetAxis("RightVertical") > 0)
+      /*  if (Mathf.Abs(Input.GetAxis("RightHorizontal")) < Mathf.Abs(Input.GetAxis("RightVertical")))
         {
+            if (Input.GetAxis("RightVertical") < 0)
+            {
+                Debug.Log("up");
+                animator.SetFloat("Vertical", Input.GetAxis("RightVertical"));
+            }
+            else if (Input.GetAxis("RightVertical") > 0)
+            {
+                Debug.Log("Down");
+                animator.SetFloat("Vertical", Input.GetAxis("RightVertical"));
+
+            }
+        }
+        else if (Input.GetAxis("RightHorizontal") > 0)
+        {
+                Debug.Log("right");
+                animator.SetFloat("Horizontal", Input.GetAxis("RightHorizontal"));
 
         }
-
-        if (Input.GetAxis("RightVertical") < 0)
+        else if (Input.GetAxis("RightHorizontal") < 0)
         {
+                Debug.Log("left");
+            animator.SetFloat("Horizontal", Input.GetAxis("RightHorizontal"));
 
-        }
-
-        if (Input.GetAxis("RightHorizontal") > 0)
-        {
-
-        }
-
-        if (Input.GetAxis("RightHorizontal") < 0)
-        {
-
-        }
+        }*/
     }
-}
+
+      
+    }
+
