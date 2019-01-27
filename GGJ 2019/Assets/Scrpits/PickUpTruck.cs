c2ef6842d6acc74d0aa2980e928a52c14ed1f257
Kabi <serodriguez88@gmail.com>
2019-01-27 16:30:58 -0300

    final re-contra final changes

--- GGJ 2019/Assets/Scrpits/PickUpTruck.cs
+++ GGJ 2019/Assets/Scrpits/PickUpTruck.cs
@@ -1,19 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PickUpTruck : MonoBehaviour
 {
+    public Text scoreText;
     public float truckWaitingTime = 60.0f;
 
     private bool aboutToLeave;
     private bool startTimer;
     private float timer;
-
+    public int score = 0;
     public List<PickUpableObject> pickedUpObjects = new List<PickUpableObject>();
     
 	void Start ()
     {
+        score = 0;
         pickedUpObjects = new List<PickUpableObject>();
         timer = 0;
 	}
@@ -38,5 +41,7 @@ public class PickUpTruck : MonoBehaviour
     public void AddItem(PickUpableObject obj)
     {
         pickedUpObjects.Add(obj);
+        score += obj.happiness;
+        scoreText.text = "Happiness: " + score.ToString("0");
     }
 }
